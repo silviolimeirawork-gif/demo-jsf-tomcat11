e8bc4f98595cc41d2b26ff2d28d47fd6b811593c
silviolimeirawork-gif <silvio.limeira.work@gmail.com>
2026-02-07 16:31:14 -0300

    HelloWorldBean

--- src/main/java/com/example/demo/HelloWorldBean.java
+++ src/main/java/com/example/demo/HelloWorldBean.java
@@ -1,4 +1,20 @@
 package com.example.demo;
 
+import jakarta.inject.Named;
+import jakarta.enterprise.context.RequestScoped;
+
+@Named("helloWorldBean") // Nome usado no XHTML
+@RequestScoped
 public class HelloWorldBean {
+
+    private String sayHello = "Olá, Jakarta Faces no Tomcat 11 :=) !";
+
+    // O JSF usa o getter para acessar a propriedade
+    public String getSayHello() {
+        return sayHello;
+    }
+
+    public void setSayHello(String sayHello) {
+        this.sayHello = sayHello;
+    }
 }
