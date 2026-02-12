de1a8ab5a76f6fd25c3ffa94d3b1f91d8aecad1c
silviolimeirawork-gif <silvio.limeira.work@gmail.com>
2026-02-12 20:33:25 -0300

    Conversao e Validacao

--- src/main/java/com/example/demo/model/Empresa.java
+++ src/main/java/com/example/demo/model/Empresa.java
@@ -1,6 +1,7 @@
 package com.example.demo.model;
 
 import java.io.Serializable;
+import java.math.BigDecimal;
 import java.util.Date;
 import java.util.Objects;
 
@@ -47,6 +48,9 @@ public class Empresa implements Serializable {
 	@Column(nullable = false, length = 30)
 	@Enumerated(EnumType.STRING)
 	private TipoEmpresa tipo;
+
+	@Column(precision = 10, scale = 2)
+	private BigDecimal faturamento;
 
 	public Long getId() {
 		return id;
@@ -102,6 +106,14 @@ public class Empresa implements Serializable {
 
 	public void setTipo(TipoEmpresa tipo) {
 		this.tipo = tipo;
+	}
+
+	public BigDecimal getFaturamento() {
+		return faturamento;
+	}
+
+	public void setFaturamento(BigDecimal faturamento) {
+		this.faturamento = faturamento;
 	}
 
 	@Override
